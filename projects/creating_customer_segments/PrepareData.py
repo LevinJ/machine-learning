1623318b7909b201c628750d320f0d867ea1257a
Levin Jian <jianzhirong@gmail.com>
2016-04-29 11:27:13 +0800

    remove outlier

--- projects/creating_customer_segments/PrepareData.py
+++ projects/creating_customer_segments/PrepareData.py
@@ -19,9 +19,46 @@ class PrepareData(ExploreData):
         print(self.log_samples)
 #         plt.show()
         return
+    def RemoveOutlier(self):
+        # For each feature find the data points with extreme high or low values
+        outliers = pd.DataFrame(columns = list(self.log_data.columns) + ['ol_count'])
+        for feature in self.log_data.keys():
+            
+            # TODO: Calculate Q1 (25th percentile of the data) for the given feature
+            Q1 = np.percentile(self.log_data, 25)
+            
+            # TODO: Calculate Q3 (75th percentile of the data) for the given feature
+            Q3 =np.percentile(self.log_data, 75)
+            
+            # TODO: Use the interquartile range to calculate an outlier step (1.5 times the interquartile range)
+            step = 1.5 * (Q3 - Q1)
+            
+            # Display the outliers
+            print "Data points considered outliers for the feature '{}':".format(feature)
+            print(self.log_data[~((self.log_data[feature] >= Q1 - step) & (self.log_data[feature] <= Q3 + step))])
+            outlier = self.log_data[~((self.log_data[feature] >= Q1 - step) & (self.log_data[feature] <= Q3 + step))]
+            for rowid, row in outlier.iterrows():
+                if rowid in outliers.index:
+                    curvalue = outliers.loc[rowid, 'ol_count']
+                    outliers.loc[rowid, 'ol_count'] = curvalue + 1
+                else:
+                    row = pd.DataFrame(np.array(list(row) + [1]).reshape((1,-1)), index =[rowid], columns = list(outliers.columns))
+                    outliers = pd.concat([outliers, row])
+            
+        
+        #print out data points considered as outliers for more than one feature 
+        
+        print(outliers.loc[outliers['ol_count'] >= 2])       
+        # OPTIONAL: Select the indices for data points you wish to remove
+        outliers  = []
+        
+        # Remove the outliers, if any were specified
+        self.good_data = self.log_data.drop(self.log_data.index[outliers]).reset_index(drop = True)
+        return
     def run(self):
         self.performScaling()
         self.featureRelevance(self.log_data)
+        self.RemoveOutlier()
         return
     
 
