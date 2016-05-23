5ac21a0e28a8ded075d717755408bb4fe55c470e
Levin Jian <jianzhirong@gmail.com>
2016-05-24 06:38:10 +0800

    fine tune epsilon to get best hyper parameters

--- projects/smartcab/smartcab/agent_enhanced_qtable.py
+++ projects/smartcab/smartcab/agent_enhanced_qtable.py
@@ -10,13 +10,13 @@ class FineTuneQTable():
     def __init__(self):
         self.scores = []
         return
-    def train(self, alpha, gamma):
-        self.single_run(OprationType.TRAIN, alpha, gamma)
-        return
-    def test(self, alpha=None, gamma=None):
-        self.single_run(OprationType.TEST, alpha, gamma)
-        return
-    def single_run(self, operationtype, alpha=None, gamma=None):
+#     def train(self, alpha, gamma, epsilon):
+#         self.single_run(OprationType.TRAIN, alpha, gamma, epsilon)
+#         return
+#     def test(self, alpha=None, gamma=None, epsilon=None):
+#         self.single_run(OprationType.TEST, alpha, gamma, epsilon)
+#         return
+    def single_run(self, operationtype, alpha=None, gamma=None, epsilon=None):
 #         ot = OprationType.TEST
         # Set up environment and agent
         e = Environment()  # create environment (also adds some dummy traffic)
@@ -32,26 +32,32 @@ class FineTuneQTable():
             a.setLearingRate(alpha)
         if not gamma is None:
             a.setDicountFactor(gamma)
+        a.setEpsion(epsilon)
         
         sim.run(n_trials=100) 
         if operationtype == OprationType.TEST:
-            self.scores.append((a.final_test_result, a.alpha, a.gamma, a.final_strrepresentation))
+            self.scores.append((a.final_test_result, a.alpha, a.gamma, a.epsion,a.final_strrepresentation))
         return
     def outputGridSearchResult(self):
         pd.options.display.max_colwidth = 300
-        df = pd.DataFrame(self.scores, columns=['score', 'alpha', 'gamma', 'finalstringrepresentation'])
+        df = pd.DataFrame(self.scores, columns=['score', 'alpha', 'gamma', 'epsilon','finalstringrepresentation'])
         print df
         maxVals = df['score'].max()
         df.to_csv('grid_search_result.csv')
         print "******Best Parameters: *****\n{}".format(df[df['score'] == maxVals])
         return
     def run(self):
-        alphas = np.arange(0, 1.1, 0.1)
-        gammas = np.linspace(0, 2,num=10)
+#         alphas = [0.1]
+#         gammas = [0.44]
+#         epsilons = [None]
+        alphas = [0.1, 0.5, 0.6, 0.8]
+        gammas = [0.44, 0.5,0.8, 1, 1.5, 2]
+        epsilons = [None, 0.3,0.5, 0.8, 1]
         for alpha in alphas:
             for gamma in gammas:
-                self.train(alpha, gamma)
-                self.test(alpha, gamma)
+                for epsilon in epsilons:
+                    self.single_run(OprationType.TRAIN, alpha, gamma, epsilon)
+                    self.single_run(OprationType.TEST, alpha, gamma, epsilon)
         self.outputGridSearchResult()
         return 
     
